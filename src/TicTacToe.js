c51a1e7f6b332be534e3145dc85b21f7c7b8a19b
Parth <psp90804@gmail.com>
2023-03-19 14:56:20 +0530

    set variable names uniformly

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,88 +1,86 @@
-import React, { useState } from "react";
-import ReactDOM from 'react-dom/client';
-import './TicTaCToe.css';
+import React from "react";
 
 class Square extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        const [isClickable, setClickable] = useState(true);
-        this.isClickable = isClickable;
-        this.setClickable = setClickable;
+        this.isClickable = true;
     }
+
+    setUnclickable(){
+        this.isClickable = false;
+    }
+
     render() {
-        const rownum = 0;
-        const colnum = (this.id%3 || 3);
+        const rownum = (this.props.id - (this.props.id % 3 || 3))/3 + 1;
+        const colnum = (this.props.id % 3 || 3);
         const squaredat = {
-            isClickable : this.isClickable,
-            id : this.props.id,
-            setClickable : this.setClickable
+            isClickable: this.isClickable,
+            id: this.props.id,
+            setUnclickable: () => this.setUnclickable()
         };
         return (
-            <div className="cell" onClick={() => this.props.handleClick(squaredat)} style={{gridRow: rownum, gridColumn: colnum}}>
+            <div className="cell" onClick={() => this.props.handleClick(squaredat)} style={{ gridRow: rownum, gridColumn: colnum }} key={this.props.id}>
                 {this.props.value}
             </div>
         )
     }
 }
 
-class Board extends React.Component{
-    constructor(props){
+class Board extends React.Component {
+    constructor(props) {
         super(props);
         this.Xpos = [];
         this.Opos = [];
         this.gameEnd = false;
+        this.Xmove = true;
         this.state = {
-            XMove : true,
-            boardBack : Array(9).fill("")
+            boardBack: Array(9).fill("")
         };
     }
 
-    squareConstruct(i){
+    squareConstruct(i) {
         return (
-            <Square value={this.boardBack[i]} id={i} handleClick={(sqr) => this.handleClick(sqr)}/>
+            <Square value={this.state.boardBack[i]} id={i} handleClick={(sqr) => this.handleClick(sqr)} />
         )
     }
 
-    handleClick(square){
-        if (this.gameEnd || (!square.isClickable)){
+    handleClick(square) {
+        if (this.gameEnd || (!square.isClickable)) {
             return 0;
         }
         const boardcopy = this.state.boardBack.slice();
-        const Xmove = this.state.XMove;
-        boardcopy[square.id] = (Xmove ? "X" : "O");
-        (Xmove ? this.Xpos : this.Opos).push(square.id);
-        square.setClickable(false);
+        boardcopy[square.id] = (this.Xmove ? "X" : "O");
+        (this.Xmove ? this.Xpos : this.Opos).push(square.id);
+        square.setUnclickable();
         this.checkWin();
-        if (!this.gameEnd){
-            Xmove = !Xmove;
-        }
+        this.Xmove = !this.Xmove;
         this.setState({
-            Xmove : Xmove,
-            boardBack : boardcopy
+            boardBack: boardcopy
         });
+        console.log(this.Xmove);
     }
 
-    checkWin(){
-        const winPos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+    checkWin() {
+        const winPos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
         const playerMoves = (this.state.XMove ? this.Xpos : this.Opos);
-        for (let i in winPos){
+        for (let i=0; i<8; i++) {
             let checkCounter = false;
-            for (let j in i){
-              if (playerMoves.includes(j)){
-                checkCounter = true;
-              } else {
-                checkCounter = false;
+            for (let j=0; j<3; j++) {
+                if (playerMoves.includes(winPos[i][j])) {
+                    checkCounter = true;
+                } else {
+                    checkCounter = false;
+                    break;
+                }
+            }
+            if (checkCounter) {
+                this.gameEnd = true;
                 break;
-              }
             }
-            if (checkCounter){
-              this.gameEnd = true;
-              break;
-            }
-          }
+        }
     }
 
-    render (){
+    render() {
         const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8,];
 
         return (
@@ -93,21 +91,14 @@ class Board extends React.Component{
     }
 }
 
-class Game extends React.Component{
-    render () {
-        return(
-            <section className="game">
-                <div className="board-holder">
-                    <Board />
-                </div>
-            </section>
-        )
-    }
+function Game() {
+    return (
+        <section className="game">
+            <div className="board-holder">
+                <Board />
+            </div>
+        </section>
+    )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Game />
-  </React.StrictMode>
-);
+export default Game;
