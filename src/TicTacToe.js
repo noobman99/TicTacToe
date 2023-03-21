3f09d4670d1ed3e130d74ec01c6027810f676fde
Parth <psp90804@gmail.com>
2023-03-21 12:00:57 +0530

    removed backBoard

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -4,10 +4,14 @@ class Square extends React.Component {
     constructor(props) {
         super(props);  // required before using this property
         this.isClickable = true;
+        this.state = {
+            value : ""
+        };
     }
 
-    setUnclickable(){ // to export unclickable function to handleClick
+    setUnclickable(val){ // to export unclickable function to handleClick
         this.isClickable = false;
+        this.setState({value : val});
     }
 
     render() {
@@ -17,13 +21,13 @@ class Square extends React.Component {
         const squaredat = {
             isClickable: this.isClickable,
             id: this.props.id,
-            setUnclickable: () => this.setUnclickable()
+            setUnclickable: (val) => this.setUnclickable(val)
         };
 
         return (
             // clickable - for styling and check, cellactive - for hover effect
-            <div className={`cell${this.isClickable ? " clickable" : ""}${(this.props.gameEnd || (!this.isClickable)) ? "" : " cellactive"}`} onClick={() => this.props.handleClick(squaredat)} style={{ gridRow: rownum, gridColumn: colnum }} key={this.props.id}>
-                {this.props.value}
+            <div key={String(this.props.id)} className={`cell${this.isClickable ? " clickable" : ""}${(this.props.gameEnd || (!this.isClickable)) ? "" : " cellactive"}`} onClick={() => this.props.handleClick(squaredat)} style={{ gridRow: rownum, gridColumn: colnum }}>
+                {this.state.value}
             </div>
         )
     }
@@ -35,16 +39,16 @@ class Board extends React.Component {
         this.Xpos = []; // positions of X
         this.Opos = []; // positions of O
         this.gameEnd = false;
-        this.Xmove = true;
+
         // to refresh the component upon change, boardBack was added in state
         this.state = {
-            boardBack: Array(9).fill("")
+            Xmove : true
         };
     }
 
     squareConstruct(i) {
         return (
-            <Square value={this.state.boardBack[i]} id={i} handleClick={(sqr) => this.handleClick(sqr)} gameEnd={this.gameEnd} />
+            <Square id={i} handleClick={(sqr) => this.handleClick(sqr)} gameEnd={this.gameEnd} />
         )
     }
 
@@ -54,22 +58,16 @@ class Board extends React.Component {
             return 0;
         }
         // updating the move into class variables
-        const boardcopy = this.state.boardBack.slice();
-        boardcopy[square.id] = (this.Xmove ? "X" : "O");
-        if (this.Xmove){
+        if (this.state.Xmove){
             this.Xpos.push(square.id);
         } else {
             this.Opos.push(square.id);
         }
 
-        square.setUnclickable();  // set square to unclickable
+        square.setUnclickable((this.state.Xmove ? "X" : "O"));  // set square to unclickable
         this.checkWin();  // to check if the game has ended
 
-        this.Xmove = !this.Xmove;  // switch moves
-        // update state -> refresh component
-        this.setState({
-            boardBack: boardcopy
-        });
+        this.setState({ Xmove : !this.state.Xmove});// switch moves and refresh footer
     }
 
     checkWin() {
@@ -77,7 +75,7 @@ class Board extends React.Component {
         const winPos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
         let playerMoves =[];
-        if (this.Xmove){
+        if (this.state.Xmove){
             playerMoves = this.Xpos;
         } else {
             playerMoves = this.Opos;
@@ -108,7 +106,7 @@ class Board extends React.Component {
             // footer for when game has ended
             return (
                 <div className="board-footer gameResult">
-                    <span class="winnerChar">{this.Xmove ? "O" : "X"}</span><span>Won The Game</span>
+                    <span className="winnerChar">{this.state.Xmove ? "O" : "X"}</span><span>Won The Game</span>
                 </div>
             )
         } else {
@@ -116,7 +114,7 @@ class Board extends React.Component {
             return(
                 <div className="board-footer">
                     <span id="playermove">Player Move : </span>
-                    <span id="playermoveValue"> {this.Xmove ? "X" : "O"} </span>
+                    <span id="playermoveValue"> {this.state.Xmove ? "X" : "O"} </span>
                 </div>
                 )
         }
